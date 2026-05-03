678f7f059af3b59ec9410cac1c2874e11a5759de
Deepak <db5608174@gmail.com>
2026-05-03 21:25:21 +0530

    style_html removed

--- main.py
+++ main.py
@@ -440,24 +440,7 @@ def save_word():
         data = request.get_json(force=True)
         html_content = data.get('html_content', '')
          
-        styled_html = f"""
-            <html>
-            <head>
-                <style>
-                    p, div, a {{
-                        word-wrap: break-word;
-                        overflow-wrap: break-word;
-                        word-break: break-all;
-                    }}
-                </style>
-            </head>
-            <body>
-                {html_content}
-            </body>
-            </html>
-            """
-
-        buf = html2docx(styled_html, title="ToolNovax_Document")
+        buf = html2docx(html_content, title="ToolNovax_Document")
         raw_bytes = buf.getvalue() 
 
         out_name = f"doc_{uuid.uuid4().hex}.docx"
